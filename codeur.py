88a3d0cd362e107091aac6bf0fd98b4698c7736c
Raphaël Daumas <raphdaumas@hotmail.fr>
2021-04-01 03:29:18 +0200

    Update Codeur Scrapper
    Auto detect cookie banner and close it
    Wait for user to solve captcha before continuing
    Auto detect number of pages
    Wait 1 minute before scrapping message, if Codeur.com wats us to wait

--- codeur.py
+++ codeur.py
@@ -1,8 +1,9 @@
 # https://www.codeur.com/users/406501/messages/archived?page=1
 
 # import requests
-import os
 import time
+import re
+
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
@@ -10,24 +11,43 @@ browser = webdriver.Chrome()
 
 codeurLogin = "https://www.codeur.com/users/sign_in"
 codeurMessages = "https://www.codeur.com/users/406501/messages/archived?page="
-lastIndex = 15
 
 
 def login():
     browser.get(codeurLogin)
+    loginURL = browser.current_url
+
+    # Set username + password
     browser.find_element_by_id("user_email").send_keys("admin@headcrab.fr")
     browser.find_element_by_id("user_password").send_keys("vypbu7-woqvur-zytqYd")
-    os.system('read -s -n 1 -p "Press any key to continue..."')
-    print()
+
+    # Check if cookie banner is displayed + close it
+    cookie = browser.find_element_by_class_name("accept-cookies-btn")
+    if cookie.is_displayed() and cookie.is_enabled():
+        cookie.click()
+
+    # submit login infos
     browser.find_element_by_name("commit").click()
-    os.system('read -s -n 1 -p "Press any key to continue..."')
-    print()
+
+    # Wait for user to solve captcha
+    while browser.current_url == loginURL:
+        time.sleep(1)
 
 
-def getAllMessages(index):
+def getAllMessages():
+    browser.get(codeurMessages)
+
+    # get last page
+    hrefString = browser.find_elements_by_class_name("page-link")[-1].get_attribute(
+        "href"
+    )
+    hrefString = re.sub("^.*=", "", hrefString)
+    index = int(hrefString)
+
+    # Foreach page, get all urls
     messagesLink = []
     while index > 0:
-        print("url : " + str(codeurMessages + str(index)))
+        print(f"url : {codeurMessages}{index}")
         browser.get(codeurMessages + str(index))
         response = browser.page_source
         index -= 1
@@ -38,26 +58,39 @@ def getAllMessages(index):
     return messagesLink
 
 
-def getMessagesContent(messagesLink):
-    textsArray = []
-    index = 0
-    for link in messagesLink:
-        text = ""
-        index += 1
-        print("Restant : " + str(len(messagesLink) - index))
-        print("https://www.codeur.com" + link)
-        browser.get("https://www.codeur.com" + link)
+# Get text from page
+def ExtractPageContent(link) -> str:
+    text = ""
+    try:
+        browser.get(f"https://www.codeur.com{link}")
         response = browser.page_source
-        # with open("oneMessage.txt", "r") as file:
-        #     response = file.read()
 
         soup = BeautifulSoup(response, "html.parser")
-        text += "========== " + str(soup.title.text) + " =========="
+        text += f"========== {soup.title.text} =========="
         for elem in soup.find_all("div", {"class": "comment-content"}):
             text += "\n---\n"
             text += str(elem.text)
         text += "\n---------- ---------- ----------\n\n"
+    except AttributeError:
+        print(f"page (https://www.codeur.com{link}) cannot be processed")
+
+    return text
+
+
+def getMessagesContent(messagesLink):
+    textsArray = []
+    index = 0
+    for link in messagesLink:
+        print(f"Restant : {len(messagesLink) - index}")
+        text = ExtractPageContent(link)
+
+        if not text:
+            time.sleep(60)
+            text = ExtractPageContent(link)
+
         textsArray.append(text)
+        index += 1
+
     return textsArray
 
 
@@ -67,25 +100,8 @@ def writeFile(text):
             file.write(elem)
 
 
-login()
-messages = getAllMessages(lastIndex)
-# print(messages)
-allMessages = getMessagesContent(messages)
-writeFile(allMessages)
-
-# with open("archive.txt", "r") as file:
-#         response = file.read()
-
-# while lastIndex > 14:
-#     response = requests.get(codeurMessages + str(lastIndex))
-#     print(codeurMessages + str(lastIndex))
-#     lastIndex -= 1
-# if not response.ok:
-#     print("No response. Quit")
-#     exit()
-
-#     soup = BeautifulSoup(response.text, "html.parser")
-#     print(response.text)
-#     messages = soup.find("closed")
-
-#     print(messages)
+if "__main__" == __name__:
+    login()
+    messages = getAllMessages()
+    allMessages = getMessagesContent(messages)
+    writeFile(allMessages)
